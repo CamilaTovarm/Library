c0e6766dcf8d67db7768a353cb37405848e23dba
Maria Camila Tovar Acosta <mctovar@ucundinamarca.edu.co>
2025-05-13 12:16:24 -0500

    Folders

---
+++ Library/Context/LibraryDbContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Library.Context
+{
+    public class LibraryDbContext
+    {
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            // Primary Keys
+            
+
+            // Foreign Keys
+           
+
+            foreach (var foreignKey in modelBuilder.Model.GetEntityTypes()
+            .SelectMany(e => e.GetForeignKeys()))
+            {
+                foreignKey.DeleteBehavior = DeleteBehavior.Restrict;
+
+            }
+        }
+    }
+}
