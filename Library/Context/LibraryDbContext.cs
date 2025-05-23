161cc701507f17408c1560207eefd2d13a1f1681
Maria Camila Tovar Acosta <mctovar@ucundinamarca.edu.co>
2025-05-23 11:08:33 -0500

    Models + Program + DbContext

--- Library/Context/LibraryDbContext.cs
+++ Library/Context/LibraryDbContext.cs
@@ -10,14 +10,12 @@ namespace Library.Context
             // Primary Keys
             
 
-            // Foreign Keys
            
 
             foreach (var foreignKey in modelBuilder.Model.GetEntityTypes()
             .SelectMany(e => e.GetForeignKeys()))
             {
                 foreignKey.DeleteBehavior = DeleteBehavior.Restrict;
-
             }
         }
     }
