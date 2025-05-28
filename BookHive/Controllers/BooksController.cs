52eab0d5e74bfc83ddbe7a6d61d72fc30b886f89
Maria Camila Tovar Acosta <mctovar@ucundinamarca.edu.co>
2025-05-28 15:55:27 -0500

    get

--- BookHive/Controllers/BooksController.cs
+++ BookHive/Controllers/BooksController.cs
@@ -31,27 +31,29 @@ namespace FrontBerries.Controllers
                 string data = response.Content.ReadAsStringAsync().Result;
                 booksList = JsonConvert.DeserializeObject<List<BookViewModel>>(data);
 
-                // Obtener autores y relaciones (como ya tienes)
+                // Obtener autores directamente
                 List<AuthorViewModel> authors = GetAuthors();
 
                 // Obtener editoriales
                 List<EditorialViewModel> editorials = GetEditorials();
 
-                // Mapear autores a libros
+                // Obtener países
+                List<CountryViewModel> countries = GetCountries();
+
+                // Mapear autor, editorial y país a cada libro
                 foreach (var book in booksList)
                 {
-                    var authorIds = authorVsBooks.Where(avb => avb.BookId == book.IdBook)
-                                                .Select(avb => avb.AuthorId)
-                                                .ToList();
+                    // Mapear autor directamente (asumiendo BookViewModel tiene AuthorId)
+                    var author = authors.FirstOrDefault(a => a.AuthorId == book.AuthorId);
+                    book.AuthorNames = author != null ? new List<string> { author.AuthorName } : new List<string> { "Autor desconocido" };
 
-                    var authorNames = authors.Where(a => authorIds.Contains(a.AuthorId))
-                                             .Select(a => a.AuthorName)
-                                             .ToList();
-
-                    book.AuthorNames = authorNames;
-
+                    // Mapear editorial
                     var editorial = editorials.FirstOrDefault(e => e.EditorialId == book.EditorialId);
                     book.EditorialName = editorial != null ? editorial.EditorialName : "Editorial desconocida";
+
+                    // Mapear país
+                    var country = countries.FirstOrDefault(c => c.CountryId == book.CountryId);
+                    book.CountryName = country != null ? country.CountryName : "País desconocido";
                 }
             }
 
@@ -59,6 +61,44 @@ namespace FrontBerries.Controllers
 
             return View(activeBooks);
         }
+
+        ////////////////////////////////
+        ///////////////////////////////
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(BookViewModel newBook)
+        {
+            if (!ModelState.IsValid)
+            {
+                // Si el modelo no es válido, vuelve a mostrar el formulario con errores
+                return View(newBook);
+            }
+
+            // Serializar el objeto a JSON
+            var jsonContent = new StringContent(JsonConvert.SerializeObject(newBook), System.Text.Encoding.UTF8, "application/json");
+
+            // Enviar POST a la API para crear el libro
+            var response = _client.PostAsync(_client.BaseAddress + "/Book", jsonContent).Result;
+
+            if (response.IsSuccessStatusCode)
+            {
+                // Redirigir al listado o detalle tras crear exitosamente
+                return RedirectToAction(nameof(Books));
+            }
+            else
+            {
+                // Manejar error, mostrar mensaje o volver al formulario
+                ModelState.AddModelError(string.Empty, "Error al crear el libro en la API.");
+                return View(newBook);
+            }
+        }
+
+
+
+
+
+
+
 
         private List<EditorialViewModel> GetEditorials()
         {
@@ -82,5 +122,17 @@ namespace FrontBerries.Controllers
             return new List<AuthorViewModel>();
         }
 
+        private List<CountryViewModel> GetCountries()
+        {
+            HttpResponseMessage response = _client.GetAsync(_client.BaseAddress + "/Country").Result;
+            if (response.IsSuccessStatusCode)
+            {
+                string data = response.Content.ReadAsStringAsync().Result;
+                return JsonConvert.DeserializeObject<List<CountryViewModel>>(data);
+            }
+            return new List<CountryViewModel>();
         }
+    }
+
+ }
 
