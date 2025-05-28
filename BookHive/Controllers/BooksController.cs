3b39cf4cd92819fec8bc3095d26376295b5decf0
Maria Camila Tovar Acosta <mctovar@ucundinamarca.edu.co>
2025-05-28 10:53:42 -0500

    Books+

---
+++ BookHive/Controllers/BooksController.cs
@@ -0,0 +1,46 @@
+﻿using BookHive.Models;
+using Library.Models; // Ajusta el namespace según tu proyecto
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+
+namespace FrontBerries.Controllers
+{
+    public class BooksController : Controller
+    {
+        private readonly Uri baseAddress = new Uri("https://bookhive-heaedbaqfgbacdhw.canadacentral-01.azurewebsites.net/api");
+        private readonly HttpClient _client;
+
+        public BooksController()
+        {
+            _client = new HttpClient();
+            _client.BaseAddress = baseAddress;
+        }
+
+        [HttpGet]
+        public IActionResult Books()
+        {
+            List<BookViewModel> booksList = new List<BookViewModel>();
+
+            HttpResponseMessage response = _client.GetAsync(_client.BaseAddress + "/Book").Result;
+
+            if (response.IsSuccessStatusCode)
+            {
+                string data = response.Content.ReadAsStringAsync().Result;
+                booksList = JsonConvert.DeserializeObject<List<BookViewModel>>(data);
+
+                // Si necesitas mapear o procesar datos adicionales, hazlo aquí
+                // Ejemplo: formatear fechas, filtrar, etc.
+            }
+
+            // Enviar solo libros activos o aplicar filtros si es necesario
+            var activeBooks = booksList.Where(b => b != null).ToList();
+
+            return View(activeBooks);
+        }
+    }
+}
---
+++ BookHive/Models/BooksViewModel.cs
@@ -0,0 +1,18 @@
+﻿using Library.Models;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace BookHive.Models
+{
+    public class BookViewModel
+    {
+        public int IdBook { get; set; }
+        public string BookTitle { get; set; }
+        public string ISBN { get; set; }
+        public DateTime PublicationDate { get; set; }
+        public int PageCount { get; set; }
+        public string Editorial { get; set; }
+        public string Country { get; set; }
+        public string ImgUrl { get; set; }
+    }
+
+}
