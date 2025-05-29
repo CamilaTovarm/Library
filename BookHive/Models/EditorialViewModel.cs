2685e54dd16a505a3e17214f808af192f612c0e7
Maria Camila Tovar Acosta <mctovar@ucundinamarca.edu.co>
2025-05-29 02:18:39 -0500

    filtro editorial lista

--- BookHive/Models/EditorialViewModel.cs
+++ BookHive/Models/EditorialViewModel.cs
@@ -1,14 +1,8 @@
-﻿using Newtonsoft.Json;
-
 namespace BookHive.Models
 {
     public class EditorialViewModel
     {
-        [JsonProperty("idEditorial")]
         public int EditorialId { get; set; }
-        [JsonProperty("nameEditorial")]
         public string EditorialName { get; set; }
-        public bool State { get; set; }
     }
-
 }
