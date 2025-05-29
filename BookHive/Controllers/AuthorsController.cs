6345751839dc59d698102472f2141765e398e580
NataliaMendez03 <lnataliamendez@ucundinamarca.edu.co>
2025-05-29 11:41:41 -0500

    Update

--- BookHive/Controllers/AuthorsController.cs
+++ BookHive/Controllers/AuthorsController.cs
@@ -1,8 +1,10 @@
 ﻿using BookHive.Models;
 using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Net.Http;
+using System.Text;
 
 namespace BookHive.Controllers
 {
@@ -82,6 +84,65 @@ namespace BookHive.Controllers
             else
             {
                 TempData["errorMessage"] = $"Error al eliminar autor: {response.ReasonPhrase}";
+            }
+            return RedirectToAction("Delete");
+        }
+
+
+        [HttpGet]
+        public IActionResult Edit(int idAuthor)
+        {
+            AuthorViewModel author = null;
+
+            var response = _client.GetAsync($"/Author/{idAuthor}").Result;
+            if (response.IsSuccessStatusCode)
+            {
+                var data = response.Content.ReadAsStringAsync().Result;
+                author = JsonConvert.DeserializeObject<AuthorViewModel>(data);
+            }
+            else
+            {
+                TempData["errorMessage"] = "Error al obtener el Autor.";
+                return RedirectToAction("Index");
+            }
+
+            return View(Index);
+        }
+
+
+        [HttpGet]
+        public IActionResult Update()
+        {
+            return View();
+        }
+
+
+        // PUT: Actualizar autor
+        [HttpPut]
+        public IActionResult UpdateAuthor(int id, [FromQuery] string authorName)
+        {
+            try
+            {
+                var responseGet = _client.GetAsync($"/api/Author/{id}").Result;
+                if (!responseGet.IsSuccessStatusCode)
+                    return NotFound("Autor no encontrado");
+
+                var data = responseGet.Content.ReadAsStringAsync().Result;
+                var author = JsonConvert.DeserializeObject<AuthorViewModel>(data);
+
+                author.AuthorName = authorName;
+
+                var jsonContent = new StringContent(JsonConvert.SerializeObject(author), Encoding.UTF8, "application/json");
+                var responsePut = _client.PutAsync($"/api/Author/Update/{id}", jsonContent).Result;
+
+                if (responsePut.IsSuccessStatusCode)
+                    return Ok("Autor actualizado correctamente");
+                else
+                    return StatusCode((int)responsePut.StatusCode, "Error al actualizar autor");
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, $"Error interno: {ex.Message}");
             }
         }
     }
