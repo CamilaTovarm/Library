1aac00276abaa0cfd39183adaf23ff743442d753
Maria Camila Tovar Acosta <mctovar@ucundinamarca.edu.co>
2025-05-29 01:02:57 -0500

    editorial delete

--- BookHive/Models/EditorialViewModel.cs
+++ BookHive/Models/EditorialViewModel.cs
@@ -1,9 +1,14 @@
-﻿namespace BookHive.Models
+﻿using Newtonsoft.Json;
+
+namespace BookHive.Models
 {
     public class EditorialViewModel
     {
+        [JsonProperty("idEditorial")]
         public int EditorialId { get; set; }
+        [JsonProperty("nameEditorial")]
         public string EditorialName { get; set; }
+        public bool State { get; set; }
     }
 
 }
